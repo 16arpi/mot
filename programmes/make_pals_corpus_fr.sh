d0829e09025236d3e921b7e84acc40f2484a8b09
César <cesarpichon@gmail.com>
2025-01-06 17:06:55 +0100

    Ajout de 2 scripts

---
+++ programmes/make_pals_corpus_fr.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+SOURCE=$1
+LANG=$2
+
+if [ ! $SOURCE ] || [ ! $LANG ]
+then
+    echo "Usage : ./make_pals_corpus.sh <dossier_source> <code_langue>" >&2
+    exit
+fi
+
+if [ ! -d $SOURCE ]
+then
+    echo "Le dossier $SOURCE n'existe pas..." >&2
+    exit
+fi
+
+ls $SOURCE/$LANG-*.txt | while read -r file
+do
+    cat "$file" | while read -r line
+    do
+        echo "$line" | egrep -o "([Aa]ujourd'hui)|(\b[éèàçôöùüûïî[:alnum:]]+\'?\b)" | while read -r token
+        do
+            echo $token
+        done
+    done
+    echo ""
+done
